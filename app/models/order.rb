13dc8f1f150d5acf0320426aeab3fcf656bf4980
Jeff Shapiro <codesheff@gmail.com>
2015-05-19 15:01:41 -0400

    relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ActiveRecord::Base
+  has_one :order_profile
+  has_one :runner
 end
--- app/models/order_profile.rb
+++ app/models/order_profile.rb
@@ -1,2 +1,4 @@
 class OrderProfile < ActiveRecord::Base
+  has_many :orders
+  has_one :business
 end
