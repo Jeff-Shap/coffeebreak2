3304a702a2b0b3eb48ddbb6b5c65ede58ffb602f
Jeff Shapiro <codesheff@gmail.com>
2015-05-19 15:00:00 -0400

    added status to order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519185338) do
+ActiveRecord::Schema.define(version: 20150519185916) do
 
   create_table "businesses", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150519185338) do
     t.datetime "updated_at",       null: false
     t.integer  "order_profile_id"
     t.integer  "runner_id"
+    t.string   "status"
   end
 
   add_index "orders", ["order_profile_id"], name: "index_orders_on_order_profile_id"
